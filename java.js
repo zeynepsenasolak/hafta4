a26d23a9c7cee2d53a9038df6124d61d7ca06825
zeynepsenasolak <zoeyyssz@gmail.com>
2024-05-14 17:32:09 +0300

    a

--- java.js
+++ java.js
@@ -18,3 +18,27 @@ person.age = 31;
 person.hobbies.push("cooking");
 console.log(person.firstName);
 console.log(person.adress.city);
+
+for (let i = 0; i<5; i++) {
+    console.log(i);
+}
+for (let i= 3 ; i>-1; i--) {
+    console.log(fruits[i]);
+}
+let x= 2
+if(x>5) {
+console.log("hello");
+} else{
+console.log("i love you");
+}
+
+let age = 25;
+if(age>= 18) {
+    if (age<21) {
+        console.log( "you are an adult but not allowed to drink yet");
+    } else { 
+        console.log("you are an adult and allowed to drink");
+    }
+} else {
+    console.log("you are a minor");
+}
